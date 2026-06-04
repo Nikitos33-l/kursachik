fdb80c87335638d46647c5345cdc5607d5e9b18c
Nikitos33-l <nikitaantoncik56@gmail.com>
2026-06-05 01:39:12 +0300

    feat(gateway):add routing

--- order-service-api-common/src/main/java/org/example/order/service/api/common/config/OrderServiceApiConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.order.service.api.common.config;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@AutoConfiguration
-@EnableFeignClients(basePackages = "org.example.order.service.api.common.client")
-public class OrderServiceApiConfig {
-}
