f83c62fa7ba494b251267a43e095b37007709e2a
Nikitos33-l <nikitaantoncik56@gmail.com>
2026-06-10 19:09:25 +0300

    feat(gateway):Add swagger

--- security-service/src/main/resources/db/migration/V2__init_superadmin_and_roles.sql
+++ security-service/src/main/resources/db/migration/V2__init_superadmin_and_roles.sql
@@ -8,7 +8,7 @@ INSERT INTO users (id, email, password, workplace_id, role_id)
 VALUES (
            '00000000-0000-0000-0000-000000000001',
            'superadmin@sto.by',
-           '$2a$10$spG8jhQT/ME9djCutY0cvewYTzAlXnZH9AvHCZ5FPEofM/S2W/Sem',
+           '$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa',
            NULL,
            3
        );
